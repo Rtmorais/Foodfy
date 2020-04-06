7eeeb247496c16563efa2a219db7e13beaf1ccf7
Rtmorais <rt.morais@hotmail.com>
2020-04-06 00:15:37 -0300

    modal script, receitas.html done

--- script.js
+++ script.js
@@ -4,5 +4,12 @@ const cards = document.querySelectorAll('.card')
 for (let card of cards) {
     card.addEventListener("click", function() {
         modalOverlay.classList.add('active')
+        modalOverlay.querySelector("img").src = card.querySelector("img").src
+        modalOverlay.querySelector("h1").innerHTML = card.querySelector("h1").innerHTML
+        modalOverlay.querySelector("h2").innerHTML = card.querySelector("h2").innerHTML
     })
 }
+document.querySelector('.close-modal').addEventListener("click", function() {
+    modalOverlay.classList.remove('active')
+})
+
